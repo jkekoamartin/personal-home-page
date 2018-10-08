60bd1d430369aee94bc6ac30abb7f2e1704689ce
jkekoamartin <james.kekoa.martin@gmail.com>
2018-10-08 16:17:43 -0500

    init

---
+++ src/main/java/com/jkekoamartin/personalhomepage/PersonalHomePageApplication.java
@@ -0,0 +1,12 @@
+package com.jkekoamartin.personalhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalHomePageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalHomePageApplication.class, args);
+    }
+}
